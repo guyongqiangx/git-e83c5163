5d8fe950a4f95faa36af41377e37b16b64b06f31
Yongqiang Gu <x_forever@126.com>
2021-07-14 16:06:02 +0800

    Rocky: 给init-db.c添加注释，该文件的主要作用是创建目录".dircache/objects/{00,01,02,...,fd,fe,ff}"
    Signed-off-by: Yongqiang Gu <x_forever@126.com>

--- init-db.c
+++ init-db.c
@@ -1,10 +1,32 @@
 #include "cache.h"
 
+/*
+ * 1. 创建目录".dircache/objects/{00,01,02,...,fd,fe,ff}"
+ * $ tree .dircache
+ * .dircache
+ * └── objects
+ * 	├── 00
+ * 	├── 01
+ * 	├── 02
+ * 	...
+ * 	├── fd
+ * 	├── fe
+ * 	└── ff
+ *
+ * 257 directories, 0 files
+ */
 int main(int argc, char **argv)
 {
 	char *sha1_dir = getenv(DB_ENVIRONMENT), *path;
 	int len, i, fd;
 
+	/*
+	 * 创建名为".dircache"的文件夹, 权限模式为0700
+	 * 如果".dircache"已经存在，则mkdir会在这里失败并返回
+	 *
+	 * $ ./init-db
+	 * unable to create .dircache: File exists
+	 */
 	if (mkdir(".dircache", 0700) < 0) {
 		perror("unable to create .dircache");
 		exit(1);
@@ -16,6 +38,7 @@ int main(int argc, char **argv)
 	 * On the other hand, it might just make lookup slower and messier. You
 	 * be the judge.
 	 */
+	/* sha1_dir = getenv("SHA1_FILE_DIRECTORY")，判断环境变量"SHA1_FILE_DIRECTORY"指向的文件是否存在，其是否为目录 */
 	sha1_dir = getenv(DB_ENVIRONMENT);
 	if (sha1_dir) {
 		struct stat st;
@@ -27,6 +50,7 @@ int main(int argc, char **argv)
 	/*
 	 * The default case is to have a DB per managed directory. 
 	 */
+	/* sha1_dir=".dircache/objects"，创建该目录 */
 	sha1_dir = DEFAULT_DB_ENVIRONMENT;
 	fprintf(stderr, "defaulting to private storage area\n");
 	len = strlen(sha1_dir);
@@ -36,6 +60,21 @@ int main(int argc, char **argv)
 			exit(1);
 		}
 	}
+	/*
+	 * 在".dircache/objects"创建256个子目录，结果如下:
+	 * $ tree .dircache
+	 * .dircache
+	 * └── objects
+	 * 	├── 00
+	 * 	├── 01
+	 * 	├── 02
+	 * 	...
+	 * 	├── fd
+	 * 	├── fe
+	 * 	└── ff
+	 *
+	 * 257 directories, 0 files
+	 */
 	path = malloc(len + 40);
 	memcpy(path, sha1_dir, len);
 	for (i = 0; i < 256; i++) {

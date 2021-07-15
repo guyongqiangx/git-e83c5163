47be0d282091171e6154efc69cf3e1850a6460f9
Yongqiang Gu <x_forever@126.com>
2021-07-15 14:48:00 +0800

    Rocky: 完善 read-tree 使用示例
    Signed-off-by: Yongqiang Gu <x_forever@126.com>

--- read-tree.c
+++ read-tree.c
@@ -61,12 +61,24 @@ int main(int argc, char **argv)
  * git-e83c5163$ ./write-tree
  * cb8b8e042b2abdf1070f9f60d83f3fb9cbe204ce
  *
- * # 2. 使用 read-tree 读取 write-tree 写入的 tree 对象
+ * # 2. 使用 cat-file 提取 tree 对象到临时文件
+ * git-e83c5163$ ./cat-file cb8b8e042b2abdf1070f9f60d83f3fb9cbe204ce
+ * temp_git_file_13xlrH: tree
+ *
+ * # 3. 查看 tree 对象内容
+ * git-e83c5163$ xxd -g 1 temp_git_file_13xlrH
+ * 00000000: 31 30 30 36 34 34 20 4d 61 6b 65 66 69 6c 65 00  100644 Makefile.
+ * 00000010: b0 4f b9 9b 9a 17 6f f0 5e 03 d5 e6 e7 39 f0 a8  .O....o.^....9..
+ * 00000020: 2b 83 c5 6c 31 30 30 36 34 34 20 52 45 41 44 4d  +..l100644 READM
+ * 00000030: 45 00 66 50 25 b1 1c e8 fb 16 fa db 7d ae bf 77  E.fP%.......}..w
+ * 00000040: cb 54 a2 ae 39 a1                                .T..9.
+ *
+ * # 4. 使用 read-tree 读取 tree 对象
  * git-e83c5163$ ./read-tree cb8b8e042b2abdf1070f9f60d83f3fb9cbe204ce
  * 100644 Makefile (b04fb99b9a176ff05e03d5e6e739f0a82b83c56c)
  * 100644 README (665025b11ce8fb16fadb7daebf77cb54a2ae39a1)
  *
- * # 3. 使用 read-tree 尝试读取一个 blob 对象 (Makefile 的 blob 对象)
+ * # 5. 使用 read-tree 尝试读取一个 blob 对象 (Makefile 的 blob 对象)
  * git-e83c5163$ ./read-tree b04fb99b9a176ff05e03d5e6e739f0a82b83c56c
  * read-tree: expected a 'tree' node
  */

708fe35f077cb5479c5e1b8270752b4e8904d0c1
Yongqiang Gu <x_forever@126.com>
2021-07-17 13:06:48 +0800

    Rocky: 给 cache.h 添加注释
    Signed-off-by: Yongqiang Gu <x_forever@126.com>

--- cache.h
+++ cache.h
@@ -22,6 +22,7 @@
  * just a cache, after all.
  */
 
+/* 暂存区文件(".dircache/index")头部格式 */
 #define CACHE_SIGNATURE 0x44495243	/* "DIRC" */
 struct cache_header {
 	unsigned int signature;
@@ -45,7 +46,13 @@ struct cache_time {
  * Again - this is just a (very strong in practice) heuristic that
  * the inode hasn't changed.
  */
+/*
+ * 暂存区文件(".dircache/index")内单条记录格式
+ * 最后一个 name 成员是大小为0的数组(用作占位符, 具体依赖于名字长度),
+ * 一条 cache entry 由前面定长的部分和最后变长的部分构成, 所以总体长度不是固定的
+ */
 struct cache_entry {
+	/* 固定长度部分 */
 	struct cache_time ctime;
 	struct cache_time mtime;
 	unsigned int st_dev;
@@ -56,6 +63,7 @@ struct cache_entry {
 	unsigned int st_size;
 	unsigned char sha1[20];
 	unsigned short namelen;
+	/* 每条 cache entry 记录的 name 实际占用的长度由前面的 namelen 决定, 这里的 name[0] 就是一个大小为0的占位符 */
 	unsigned char name[0];
 };
 
@@ -63,29 +71,45 @@ const char *sha1_file_directory;
 struct cache_entry **active_cache;
 unsigned int active_nr, active_alloc;
 
+/*
+ * 版本库目录的环境变量和默认名称, 当前git版本库目录已经改为".git/objects"了
+ */
 #define DB_ENVIRONMENT "SHA1_FILE_DIRECTORY"
 #define DEFAULT_DB_ENVIRONMENT ".dircache/objects"
 
+/*
+ * 根据传入name字符串的长度len, 计算所在cache entry的大小(8字节对齐)
+ */
 #define cache_entry_size(len) ((offsetof(struct cache_entry,name) + (len) + 8) & ~7)
+/*
+ * 根据传入cache entry的记录, 计算cache entry的大小
+ */
 #define ce_size(ce) cache_entry_size((ce)->namelen)
 
 #define alloc_nr(x) (((x)+16)*3/2)
 
 /* Initialize the cache information */
+/* 读取索引文件".dircache/index", 建立缓存, 返回条目数 */
 extern int read_cache(void);
 
 /* Return a statically allocated filename matching the sha1 signature */
+/* 获取 sha1 值对应的文件名 */
 extern char *sha1_file_name(unsigned char *sha1);
 
 /* Write a memory buffer out to the sha file */
+/* 将 buf 中大小为 size 的数据写入到 sha1 值对应的文件中 */
 extern int write_sha1_buffer(unsigned char *sha1, void *buf, unsigned int size);
 
 /* Read and unpack a sha1 file into memory, write memory to a sha1 file */
+/* 提取 sha1 值对应文件的内容(解压缩后返回), 返回内容类型(blob/tree/commit)和 size */
 extern void * read_sha1_file(unsigned char *sha1, char *type, unsigned long *size);
+/* 压缩 buf 数据, 计算 sha1 值, 并写入对应的 sha1 文件中 */
 extern int write_sha1_file(char *buf, unsigned len);
 
 /* Convert to/from hex/sha1 representation */
+/* 将 sha1 字符串转换成相应的 sha1 值 */
 extern int get_sha1_hex(char *hex, unsigned char *sha1);
+/* 将 sha1 值转换成相应的 sha1 字符串 */
 extern char *sha1_to_hex(unsigned char *sha1);	/* static buffer! */
 
 /* General helper functions */
